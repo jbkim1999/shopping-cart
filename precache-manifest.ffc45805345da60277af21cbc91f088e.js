e4213f1cd41820d08b35386ef61f67776ab197fb
jbkim1999 <jungbae.kim@u.nus.edu>
2022-01-11 18:58:40 +0800

    Updates

---
+++ precache-manifest.ffc45805345da60277af21cbc91f088e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37a90d880261616493910c068234e05a",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "4e8dd373aaed7a48aae3",
+    "url": "/shopping-cart/static/css/main.79e66cab.chunk.css"
+  },
+  {
+    "revision": "1fcd783e934ac2245c2e",
+    "url": "/shopping-cart/static/js/2.717e5b76.chunk.js"
+  },
+  {
+    "revision": "4e8dd373aaed7a48aae3",
+    "url": "/shopping-cart/static/js/main.7c361e00.chunk.js"
+  },
+  {
+    "revision": "3675f54918f9d621420d",
+    "url": "/shopping-cart/static/js/runtime~main.60a85d2f.js"
+  }
+]);
