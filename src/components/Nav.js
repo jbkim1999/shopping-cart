b388c5e2ef8d67bbd9115236b1c1ff9b9daac2d1
jbkim1999 <jungbae.kim@u.nus.edu>
2022-01-11 19:33:09 +0800

    Still fixing subdirectories

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function Nav() {
   return (
     <nav>
-      <Link to="/shopping-cart">Home</Link>
+      <Link to="/shopping-cart/">Home</Link>
       <Link to="/shopping-cart/shopping">Shopping</Link>
       <Link to="/shopping-cart/cart">Cart</Link>
     </nav>
